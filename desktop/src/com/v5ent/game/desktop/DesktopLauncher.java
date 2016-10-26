2ec794c7a15cabe51e670a52d03b6fe81de09d32
Administrator <Administrator@XN043798>
2016-10-26 15:56:09 +0800

    initial gdx setup

---
+++ desktop/src/com/v5ent/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.v5ent.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.v5ent.game.SomeWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SomeWars(), config);
+	}
+}
