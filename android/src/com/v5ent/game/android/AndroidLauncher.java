8527896743b0ef94f0bf7e1c9f41132b3ecfff38
Administrator <Administrator@XN043798>
2016-10-27 19:00:26 +0800

    android version

--- android/src/com/v5ent/game/android/AndroidLauncher.java
+++ android/src/com/v5ent/game/android/AndroidLauncher.java
@@ -1,16 +1,17 @@
 package com.v5ent.game.android;
 
-import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.v5ent.game.SomeWars;
+import com.v5ent.game.ScrollPaneTest;
+
+import android.os.Bundle;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SomeWars(), config);
+//		initialize(new SomeWars(), config);
+		initialize(new ScrollPaneTest(), config);
 	}
 }
