fd3b66fff7c39d46e63222ee7562aaaa87394f1a
Administrator <Administrator@XN043798>
2016-10-27 16:45:12 +0800

    add asserts

--- desktop/src/com/v5ent/game/desktop/DesktopLauncher.java
+++ desktop/src/com/v5ent/game/desktop/DesktopLauncher.java
@@ -1,7 +1,5 @@
 package com.v5ent.game.desktop;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.v5ent.game.MultiplayerDemo;
@@ -9,10 +7,7 @@ import com.v5ent.game.MultiplayerDemo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		new LwjglApplication(new ScrollPaneTest(), config);
-		Gdx.app = new LwjglApplication(new MultiplayerDemo(), config);
-		// Set Libgdx log level
-		Gdx.app.setLogLevel(Application.LOG_DEBUG);
-
+//		new LwjglApplication(new SomeWars(), config);
+		new LwjglApplication(new MultiplayerDemo(), config);
 	}
 }
